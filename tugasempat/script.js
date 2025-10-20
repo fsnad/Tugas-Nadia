3cc380527b8a625f45d388c1c377f38a392bcec7
nadfay <nadiafairuzsyifa@gmail.com>
2025-10-20 19:08:52 +0700

    upload

---
+++ tugasempat/script.js
@@ -0,0 +1,12 @@
+const tombolMusik = document.getElementById("tombolMusik");
+const musik = document.getElementById("musik");
+
+tombolMusik.addEventListener("click", () => {
+    if (musik.paused) {
+        musik.play();
+        tombolMusik.textContent = "⏸️ Pause Famous";
+    } else {
+        musik.pause();
+        tombolMusik.textContent = "▶️ Play Famous";
+    }
+});
